6bf6c085719080aec35d496ff2e3df511f9ae82d
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-08-01 13:59:58 +0300

    created config.js

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const app = require('./app');
 
-const DB_HOST =
-  'mongodb+srv://Eujinn:QZqQtLkTaKRcG9Uf@cluster0.v4ll3w5.mongodb.net/db-contacts?retryWrites=true&w=majority';
+const { DB_HOST } = require('./config');
 
 mongoose.set('strictQuery', true);
 
