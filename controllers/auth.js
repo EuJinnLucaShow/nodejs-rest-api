b70dd3c5a85deba866ad83206ce638591ca7da56
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-08-05 14:38:53 +0300

    Logout success response Status: 204 No Content

--- controllers/auth.js
+++ controllers/auth.js
@@ -60,9 +60,7 @@ const logout = async (req, res) => {
   if (!result) {
     throw HttpError(404, 'Not found');
   }
-  res.json({
-    message: 'Logout success',
-  });
+  res.status(204).json({});
 };
 
 module.exports = {
